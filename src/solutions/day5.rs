57efa87373e2e40663bb1fe6f09b3ef5ba7e1047
Dima <dmyt.ov@gmail.com>
2024-04-12 23:14:38 +0300

    Day 5: Satisfying the borrow checker to avoid unnecessary slice materialization!

--- src/solutions/day5.rs
+++ src/solutions/day5.rs
@@ -49,18 +49,31 @@ impl CrateStacks {
 
         CrateStacks { stacks }
     }
+    
+    fn get_stacks_for_op_mut(&mut self, op: &MoveOp) -> (&mut Vec<char>, &mut Vec<char>) {
+        let (src, dst) = if op.from < op.to { 
+            let (left, right) = self.stacks.split_at_mut(op.to);
+            (&mut left[op.from], right.first_mut().unwrap())
+        } else {
+            let (left, right) = self.stacks.split_at_mut(op.from);
+            (right.first_mut().unwrap(), &mut left[op.to])
+        };
+
+        (src, dst)
+    }
 
     fn execute(&mut self, op: MoveOp, preserve_order: bool) {
+        let (src, dst) = self.get_stacks_for_op_mut(&op);
+        
         if preserve_order {
-            let new_src_len = self.stacks[op.from].len() - op.count;
-            let sub_stack = self.stacks[op.from][new_src_len..].to_vec();
+            let new_src_len = src.len() - op.count;
+            let sub_stack = &src[new_src_len..];
 
-            self.stacks[op.to].extend_from_slice(sub_stack.as_slice());
-            self.stacks[op.from].truncate(new_src_len);
+            dst.extend_from_slice(sub_stack);
+            src.truncate(new_src_len);
         } else {
             for _ in 0..op.count {
-                let val = self.stacks[op.from].pop().unwrap();
-                self.stacks[op.to].push(val)
+                dst.push(src.pop().unwrap())
             }
         }
     }
