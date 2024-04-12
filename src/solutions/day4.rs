50c6656c83394c4af04cb9a0e06f8ee3d41692c4
Dima <dmyt.ov@gmail.com>
2024-04-12 14:11:46 +0300

    Day 4

---
+++ src/solutions/day4.rs
@@ -0,0 +1,50 @@
+use crate::Stage;
+use std::error::Error;
+use std::ops::RangeInclusive;
+use std::str::FromStr;
+
+pub fn solve(stage: Stage, input: Vec<String>) -> i64 {
+    let pairs = input.iter().map(|x| x.parse().unwrap());
+
+    pairs.filter(|x: &JobPair| match stage {
+        Stage::Easy => x.is_full_overlap(),
+        Stage::Hard => x.is_any_overlap()
+    }).count() as i64
+}
+
+struct JobPair {
+    first: RangeInclusive<i32>,
+    second: RangeInclusive<i32>,
+}
+
+impl JobPair {
+    fn is_full_overlap(&self) -> bool {
+        self.first.contains(self.second.start()) && self.first.contains(self.second.end())
+            || self.second.contains(self.first.start()) && self.second.contains(self.first.end())
+    }
+
+    fn is_any_overlap(&self) -> bool {
+        self.first.contains(self.second.start())
+            || self.first.contains(self.second.end())
+            || self.second.contains(self.first.start())
+            || self.second.contains(self.first.end())
+    }
+}
+
+impl FromStr for JobPair {
+    type Err = Box<dyn Error>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<&str> = s.split(',').collect();
+
+        let parse_range = |s: &str| -> Result<RangeInclusive<i32>, Self::Err> {
+            let parts: Vec<&str> = s.split('-').collect();
+            Ok(RangeInclusive::new(parts[0].parse()?, parts[1].parse()?))
+        };
+
+        Ok(JobPair {
+            first: parse_range(parts[0])?,
+            second: parse_range(parts[1])?,
+        })
+    }
+}
